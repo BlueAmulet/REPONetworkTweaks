42ecf4268d0cbed2258e5985bd421c40bdb3376b
BlueAmulet <43395286+BlueAmulet@users.noreply.github.com>
2025-03-26 12:14:58 -0600

    1.0.2
    Ignore velocity data when attached to a hinge
    Take into account acceleration and rotation when projecting into the future
    Make timing threshold configurable
    Move photon updates to LateUpdate for faster update rate
    	Also fixes 60Hz monitors
    Add limits to config options for REPOConfig

---
+++ Patches/PhotonHandlerPatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+
+namespace REPONetworkTweaks.Patches
+{
+	[HarmonyPatch(typeof(PhotonHandler))]
+	internal static class PhotonHandlerPatch
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch(nameof(PhotonHandler.Awake))]
+		public static void PrefixAwake()
+		{
+			// Cannot touch PhotonNetwork early or else it breaks, so do it here in PhotonHandler.Awake instead
+			if (Settings.PhotonLateUpdate.Value)
+			{
+				REPONetworkTweaks.Log.LogInfo("Changing PhotonHandler to run in LateUpdate");
+				PhotonNetwork.MinimalTimeScaleToDispatchInFixedUpdate = float.PositiveInfinity;
+			}
+		}
+	}
+}
--- thunderstore/CHANGELOG.md
+++ thunderstore/CHANGELOG.md
@@ -1,3 +1,10 @@
+# 1.0.2
+- Fix doors disconnecting from their hinge on movement
+- Better future predictions
+- Process network packets faster
+- Fixed 60Hz jitter
+- Added limits to config options
+
 # 1.0.1
 - Fix broken single player
 
